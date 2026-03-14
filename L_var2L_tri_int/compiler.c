6e946534fe7d75d2770ee23dd5aae8ae80e7d56b
lenoz <lenoz@thelenoz.me>
2026-03-14 11:44:22 +0300

    first compiler

--- L_var2L_tri_int/compiler.c
+++ L_var2L_tri_int/compiler.c
@@ -1,45 +1,97 @@
+#include "../parser/parser.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int compile_buffer(const char *source_buffer, char **result_buffer, size_t *result_size)
+#define MAX_TMP_VAR_NAME_LEN 31
+
+int compile_expr(AST *expr, const char *result_name, FILE *out_fp, int *var_name_c)
 {
-  
+  const char *op_c, *l_arg, *r_arg;
+  char l_arg_val[MAX_TMP_VAR_NAME_LEN + 1];
+  char r_arg_val[MAX_TMP_VAR_NAME_LEN + 1];
+  switch (expr->type)
+  {
+  case SEXP:
+    if (!strcmp(expr->lchild->value, "let"))
+    {
+      compile_expr(expr->rchild->lchild->rchild->lchild, expr->rchild->lchild->lchild->value, out_fp, var_name_c);
+      compile_expr(expr->rchild->rchild->lchild, result_name, out_fp, var_name_c);
+    }
+    else
+    {
+      if ((*expr->lchild->value == '+' || *expr->lchild->value == '-' ||
+           *expr->lchild->value == '*' || *expr->lchild->value == '/'))
+      {
+        if (expr->rchild->lchild->type == CONST_NUM || expr->rchild->lchild->type == NAME)
+          l_arg = expr->rchild->lchild->value;
+        else
+        {
+          l_arg = l_arg_val;
+          memset(l_arg_val, 0, MAX_TMP_VAR_NAME_LEN + 1);
+          snprintf(l_arg_val, MAX_TMP_VAR_NAME_LEN, "__tmp_%d", (*var_name_c)++);
+          compile_expr(expr->rchild->lchild, l_arg, out_fp, var_name_c);
+        }
+        if (expr->rchild->rchild->lchild->type == CONST_NUM || expr->rchild->rchild->lchild->type == NAME)
+          r_arg = expr->rchild->rchild->lchild->value;
+        else
+        {
+          r_arg = r_arg_val;
+          memset(r_arg_val, 0, MAX_TMP_VAR_NAME_LEN + 1);
+          snprintf(r_arg_val, MAX_TMP_VAR_NAME_LEN, "__tmp_%d", (*var_name_c)++);
+          compile_expr(expr->rchild->rchild->lchild, r_arg, out_fp, var_name_c);
+        }
+
+        fprintf(out_fp, "(let %s (%s %s %s))\n", result_name,
+                expr->lchild->value, l_arg, r_arg);
+      }
+      else
+      {
+        return fprintf(stderr, "Fatal: unsupported operator: '%s'\n", expr->value), -1;
+      }
+    }
+
+    break;
+  default:
+    return fprintf(stderr, "Fatal: compilation flow shouldn't be here. '%s'\n", expr->value), -1;
+  }
   return 0;
-}    
+}
+
+int compile_program(AST *program, FILE *out_fp)
+{
+  if (!program || !out_fp)
+    return fprintf(stderr, "Fatal: no program or file_ptr.\n"), -1;
+
+  int var_name_c = 0;
+
+  putc('(', out_fp);
+
+  int status = compile_expr(program->lchild->lchild, "res", out_fp, &var_name_c);
+
+  putc(')', out_fp);
+  return status;
+}
 
 int compile_file(const char *path, const char *out_path)
 {
   FILE *fp = fopen(path, "r");
-
   if (!fp)
     return fprintf(stderr, "Fatal: can't open file '%s'\n", path), -1;
 
-  fseek(fp, 0L, SEEK_END);
-  size_t file_size = ftell(fp);
-  fseek(fp, 0L, SEEK_SET);
+  AST *program = parse_stream(fp, 1); // close inside
 
-  char *buffer = malloc(file_size * sizeof(char));
+  fp = fopen(out_path, "w");
+  if (!fp)
+    return fprintf(stderr, "Fatal: can't open file '%s'\n", out_path), -1;
 
-  fread(buffer, sizeof(char), file_size, fp);
+  int status = compile_program(program, fp);
+
   fclose(fp);
-
-  char *result;
-  size_t result_size;
-  int status = compile_buffer(buffer, &result, &result_size);
-  free(buffer);
+  D_AST(program);
 
   if (status != 0)
     return status;
 
-  if (!result)
-    return fprintf(stderr, "No compilation result, exiting.\n"), -1;
-
-  fp = fopen(out_path, "w");
-  if (!fp)
-    return fprintf(stderr, "Fatal: can't open file '%s'\n", path), -1;
-  fwrite(result, sizeof(char), result_size, fp);
-  fclose(fp);
-  free(result);
   return 0;
 }
 
